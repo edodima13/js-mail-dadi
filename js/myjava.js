3176da1badf1819e981750a726cfa3901098519b
edodima13 <edodima13@libero.it>
2022-02-09 17:01:58 +0100

    avanzmento e fine esercizio dei dadi

--- js/myjava.js
+++ js/myjava.js
@@ -2,20 +2,44 @@
 
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
-stampa un messaggio appropriato sull’esito del controllo.
+stampa un messaggio appropriato sull’esito del controllo.*/
 
-Gioco dei dadi
 
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi ha il numero più alto.  (e se pari?)*/
 
 //chiedere la mail all'utente
 const email = prompt("inserisci la tua email")
 
 //fare una lista di email da accettare
-const emailRight = ("tizio@gmail.com","caio@hotmail.it","sempronio@libero.it")
- 
-//stampare un messaggio che informa che i dati inseriti non sono corretti
+const emailRight = ["tizio@gmail.com","caio@hotmail.it","sempronio@libero.it"]
+
+
 if ( email != emailRight ) {
-    alert("la tua email non è valida riprova")
+    console.log("riprova")
+} else {
+    console.log("i dati sono corretti")
+}
+//stampare un messaggio che informa che i dati inseriti sono corretti
+
+
+
+
+/*Gioco dei dadi
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi ha il numero più alto.  (e se pari?)*/
+
+//generare un numero random per l'utente
+let userNumber = Math.floor(Math.random() * 5) +1;
+
+//generare un numero random per il computer
+let computerNumber = Math.floor(Math.random() * 5) +1;
+
+console.log(userNumber, computerNumber)
+
+if (userNumber < computerNumber){
+    console.log("hai perso, mi dispiace...")
+} else if (userNumber > computerNumber){
+    console.log("hai vinto, complimenti!")
+} else{
+    console.log("pareggio")
 }
