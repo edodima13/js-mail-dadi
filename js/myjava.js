efb87d650f07a389f242f49d69a5db89ca8a945f
edodima13 <edodima13@libero.it>
2022-02-09 15:34:57 +0100

    inizio esercizio

---
+++ js/myjava.js
@@ -0,0 +1,16 @@
+/*Mail
+
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+Gioco dei dadi
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi ha il numero più alto.  (e se pari?)*/
+
+//chiedere la mail all'utente
+const email = prompt("inserisci la tua email")
+
+//fare una lista di email da accettare
+//stampare un messaggio che informa che i dati inseriti sono corretti
